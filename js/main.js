0ae7ba8fd4f2ce2b57fcac0f27b5c7ea3f892f85
Jonathan Trujillo <jonathantruest@gmail.com>
2019-06-09 09:38:33 +0200

    MapBox key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoiam9uYXRoYW50ZSIsImEiOiJjandvbHpxeXQyM204NDRwN2pxa2Y0YXVyIn0.Xa7Ic1FDUgKLuMAyh9pLnA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
